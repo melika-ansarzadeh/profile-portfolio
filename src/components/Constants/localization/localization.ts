a5f32dba1f663e27a7918966b0fb42b59862c9f5
MELIKA <DESKTOP-3DTMGT6>
2025-10-29 06:20:31 +0330

    feat:create footer components

--- src/components/Constants/localization/localization.ts
+++ src/components/Constants/localization/localization.ts
@@ -3,3 +3,7 @@ export const headerlocalization = {
   mySkills: 'my skills',
   myProjects: 'my projects',
 };
+
+export const footerlocalization = {
+  text: '2025 All rights reserved. Designed by Melika Ansarzadeh.',
+};
--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -1,3 +1,9 @@
+import { footerlocalization } from '@/components/Constants/localization/localization';
+
 export default function Footer() {
-  return <div></div>;
+  return (
+    <div className="bg-custom-100 text-xs text-center p-3">
+      <p className="text-custom-400 font-semibold">{footerlocalization.text}</p>
+    </div>
+  );
 }
